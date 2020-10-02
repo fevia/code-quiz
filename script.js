2fb9c01bf2e53461f7ca44925981415559c24986
Flory Ann Evia <floryann.evia@gmail.com>
2020-10-01 20:16:29 -0500

    adding question

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById ('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
+
 
 startButton.addEventListener('click', startGame)
 
@@ -7,13 +11,32 @@ startButton.addEventListener('click', startGame)
 function startGame(){
     // console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion(){
-
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+}
+
 
 function selectAnswer(){
 
 }
+
+const questions = [
+    {
+        question:' What is 20 + 20? ',
+        answers: [
+            { text: '60', correct: false },
+            { text: '20', correct: true }
+        ]
+    }
+]
