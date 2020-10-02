b4051c0c54d1a35ba2bff4e3b6d8b19dbd5a4f9f
Flory Ann Evia <floryann.evia@gmail.com>
2020-10-01 20:00:35 -0500

    created startgame

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
 startButton.addEventListener('click', startGame)
 
 
 function startGame(){
-    console.log('Started')
+    // console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
 }
 
 function setNextQuestion(){
