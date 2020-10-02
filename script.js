acb282ba8f100ffc5627d851fb5cb79a1c59ca6f
Flory Ann Evia <floryann.evia@gmail.com>
2020-10-01 20:29:09 -0500

    trying to shuffle questions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById ('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+
 let shuffledQuestions, currentQuestionIndex
 
 
@@ -9,7 +10,7 @@ startButton.addEventListener('click', startGame)
 
 
 function startGame(){
-    // console.log('Started')
+    console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -21,13 +22,22 @@ function setNextQuestion(){
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-
 function showQuestion(question){
     questionElement.innerText = question.question
+    questions.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    });
 }
 
 
-function selectAnswer(){
+function selectAnswer(e){
 
 }
 
