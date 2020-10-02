1a5b2bbd741324ee9357b7d19ebfd2bfc176fa10
Flory Ann Evia <floryann.evia@gmail.com>
2020-10-01 19:43:08 -0500

    implement start button click and functions

---
+++ script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+
+function startGame(){
+
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
